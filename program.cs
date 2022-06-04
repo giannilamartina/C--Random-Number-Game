7a127bd1345b923d4e29f58988a3f3c7e42205de
Gianni LaMartina <lamartinagianni@gmail.com>
2022-06-04 02:29:51 -0500

    first commit

---
+++ program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberGuessingGame {
+    class Program {
+        static void Main(String[] args) {
+            Random r = new Random();
+
+            int winNum = r.Next(1, 100);
+
+            bool win = false;
+
+            do {
+                Console.Write("Guess a number inbetween 0 and 100: ");
+                string s = Console.ReadLine();
+                
+                int i = int.Parse(s);
+
+                if (i > winNum) {
+                    Console.WriteLine("Too high! Guess lower....");
+                } else if (i < winNum) {
+                    Console.WriteLine("Too low! Guess higher....");
+                } else if (i == winNum) {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+            } while (win == false);
+        }
+    }
+}
